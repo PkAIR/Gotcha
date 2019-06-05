6cf2b9bc1eafc350244e73f6544e94112a1604a8
Pavel Kalyugin <kalyuginpavel@gmail.com>
2019-06-05 16:16:08 +0300

    Update main.spec.js
    Adds data file usage

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -1,53 +1,59 @@
 import 'cypress-xpath'
+
 describe('Sending new report', function() {
+    let data = cy.readFile("cypress/data/data.json") // as?
+    
     it('Navigating to the Report side', () => {
         cy.visit('https://xn--90adear.xn--p1ai/request_main')
         
+        cy.log('Checkilng an agreement checkbox')
         cy.get('div.f-left.checkError > b > label').click()
+        cy.log('Going to the next step')
         cy.get('div.f-right.u-form > button').click()
     })
 
     it('Choosing region and department', () => {   
         cy.xpath('(//td[contains(., "Регион")])[1]/ancestor::tr/td[2]/select')
-            .select('78 г. Санкт-Петербург', {force: true})
+            .select(data.its('region'), {force: true})
+        cy.log('Region was set')
         cy.xpath('(//td[contains(., "Подразделение")])[1]/ancestor::tr/td[2]/select')
-            .select('УГИБДД ГУ МВД России по г. Санкт-Петербургу и Ленинградской области', {force: true})
+            .select('(data.its('department')', {force: true})
+        cy.log('Department was set too')        
     })
 
     it('Setting personal information', () => {
-        cy.get('#surname_check')
-            .type('Калюгин').should('have.value', 'Калюгин')
-        cy.get('#firstname_check')   
-            .type('Павел').should('have.value', 'Павел')
-        cy.get('#email_check').type('test.test@gmail.com')
+        cy.get('#surname_check').type(data.its('surname'))
+        cy.get('#firstname_check').type((data.its('name'))
+        cy.get('#email_check').type((data.its('email'))
         /*cy.xpath('//*[@name="region"]', {force: true})
             .select('г. Санкт-Петербург')*/
+        cy.get('#select2-event_region-ia-container').select((data.its('placeOfAcc'))
     })
 
     it('Filling the reason', () => {
-        // Filling reason
         cy.get('.textarea').type('Temp reason')    
     })
 
     it('Wait for file to be uploaded', () => {
-        cy.get('#fileupload-input').click({force: true})
-        // Set up request timeout?
+        cy.get('#fileupload-input').click()
+        cy.log('Upload necessary file please')
         cy.wait(10000)
+        cy.log('Waiting for file to be uploaded finally')        
+        cy.get('.half_link', { timeout: 15000 })
         
-        cy.get('.half_link')
-        
-        // Waiting for me to insert captcha
-        cy.wait(30000)
+        cy.log('Attention! Please provide captcha. Waiting for your input')
     })
     
     it('Passing mailbox check', () => {
-        cy.get('#confirm_but').click()
-        // Waiting for code entry
-        cy.wait(15000)
+        cy.get('#confirm_but:first-child', { timeout: 20000 }).click()
+        cy.log('Attention! Please provide e-mail confirmation code. Waiting for your input')
+        cy.get('#success > img', {timeout: 20000})
+        cy.log('Mail was confirmed')
     })
 
     it('Checking correctness for the report', () => {
-        cy.xpath('//label[text()="Корректность введенных данных подтверждаю"]').click()
+        //cy.xpath('//label[text()="Корректность введенных данных подтверждаю"]').click()
+        cy.get('#correct:first-child').click()
     })
 
     it('Sending the report', () => {
